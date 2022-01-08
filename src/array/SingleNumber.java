3ab207f2cb3dbbc88f6ff78e6cdc094662790e18
Commissar94 <andreykomissarov94@gmail.com>
2022-01-08 12:49:45 +0300

    + new

---
+++ src/array/SingleNumber.java
@@ -0,0 +1,35 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] nums = new int[]{4,1,2,1,2};
+        SingleNumber singleNumber = new SingleNumber();
+        System.out.println(singleNumber.singleNumber(nums));
+    }
+
+    public int singleNumber(int[] nums) {
+
+        int result = 0;
+        for(int num: nums){
+            result = result ^ num;
+        }
+        return result;
+
+//        List<Integer> numsList = new ArrayList();
+//        for (int num : nums) {
+//            numsList.add(num);
+//        }
+//
+//        return Arrays.stream(Arrays.stream(nums)
+//                        .filter(num -> Collections.frequency(numsList, num) == 1)
+//                        .toArray())
+//                .findFirst()
+//                .getAsInt();
+    }
+}
