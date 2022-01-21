d09a63b09e62b7be8fc66a373f0cd4907404e8f1
Commissar94 <andreykomissarov94@gmail.com>
2022-01-21 10:39:11 +0300

    + new

---
+++ src/noCategory/RomanToInteger.java
@@ -0,0 +1,59 @@
+package noCategory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> roman = new HashMap<>() {{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+
+        char[] digits = s.toCharArray();
+        int sum = 0;
+
+        for (int i = 0; i < digits.length; i++) {
+            if (i < digits.length-1 && (roman.get(digits[i]) < roman.get(digits[i + 1]))) {
+                sum = sum + (roman.get(digits[i + 1]) - roman.get(digits[i]));
+                i++;
+            } else {
+                sum += roman.get(digits[i]);
+            }
+
+        }
+        return sum;
+    }
+}
+
+/*
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+ */
