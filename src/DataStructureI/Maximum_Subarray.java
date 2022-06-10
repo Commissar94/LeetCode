0af02103771d75629b8dc778cb12162cb86f733a
Commissar94 <andreykomissarov94@gmail.com>
2022-06-11 00:45:52 +0300

    +

--- src/DataStructureI/Maximum_Subarray.java
+++ src/DataStructureI/Maximum_Subarray.java
@@ -27,15 +27,15 @@ public class Maximum_Subarray {
 
     public static int maxSubArray(int[] nums) {
 
-        int sum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        for (int num : nums) {
-            sum += num;
-            maxSum = Math.max(maxSum, sum);
-            if (sum < 0) {
-                sum = 0;
-            }
+        int max = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int number : nums) {
+            currentSum += number;
+            max = Math.max(max, currentSum);
+            if (currentSum < 0) currentSum = 0;
         }
-        return maxSum;
+
+        return max;
     }
 }
